34a47e376196fe2829283d54d3ff432c6ab0a8c0
Travis Webb <me@traviswebb.com>
2014-04-22 06:01:34 +0000

    fixed dupe entry

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var fs = require('fs');
 var path = require('path');
-var npmrcFile = path.resolve(require('home-dir').directory, '.npmrc');
-var registryEntry = 'registry = https://registry.npmjs.us/public';
+var npmrcPath = path.resolve(require('home-dir').directory, '.npmrc');
+var registryEntry = [
+  '',
+  '; npmjs.us registry directive added automatically by the npmjs.us module',
+  'registry = https://registry.npmjs.us/public',
+  ''
+].join('\n');
+var npmrcFile;
 
 try {
-  fs.appendFileSync(npmrcFile, registryEntry);
+  npmrcFile = fs.readFileSync(npmrcPath).toString();
 }
 catch (e) {
-  throw new Error('Sorry! I was unable to add the npmjs.us registry to your .npmrc; ' + e.message);
+  // file doesn't exist; that's fine, we're about to create it anyway
+}
+try {
+  if (!/npmjs.us registry directive/.test(npmrcFile)) {
+    fs.appendFileSync(npmrcPath, registryEntry);
+  }
+}
+catch (e) {
+  throw new Error('Sorry! I was unable to add the npmjs.us registry to your ~/.npmrc; ' + e.message);
 }
